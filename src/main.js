6a4e717c5fe11644d06e346097303360285b421d
onehousesilver <onehousesilver@naver.com>
2022-04-15 13:54:29 +0900

    test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import router from "./router";
+import App from "./App.vue";
+Vue.config.productionTip = false;
+Vue.config.ignoredElements = [
+  "a-scene",
+  "a-entity",
+  "a-camera",
+  "a-box",
+  "a-sky",
+  "a-sphere",
+  "a-cylinder",
+  "a-plane",
+  "a-assets",
+  "a-asset-item",
+];
+import aframe from "aframe";
+Vue.use(aframe);
 
-Vue.config.productionTip = false
+// import * as THREE from "three";
+// Vue.use(THREE);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
